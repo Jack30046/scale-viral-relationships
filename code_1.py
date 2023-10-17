85bae4432b3c3549d1dcfe86cf7663b31d0a4a3c
Jack <143327277+Jack30046@users.noreply.github.com>
2023-10-17 15:43:39 +0100

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,43 +1,52 @@
-def main():
-
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+if __name__ == "__main__":
 import random
     return data
-        print(f"Random Number: {item}")
-    main()
+def main():
+
 def generate_random_data():
+    data = generate_random_data()
+    for item in data:
+
+
+    return data
+        print(f"Random Number: {item}")
+def generate_random_data():
+
+if __name__ == "__main__":
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+
+
+def main():
 
     for item in data:
-    data = [random.randint(1, 100) for _ in range(10)]
-    data = generate_random_data()
-if __name__ == "__main__":
-
-    data = [random.randint(1, 100) for _ in range(10)]
-
-    return data
-def generate_random_data():
 import random
-    data = generate_random_data()
+    main()            data[i] = (int) (Math.random() * 100) + 1;
+}
+        for (int item : data) {
+    }
+            System.out.println("Random Number: " + item);
 
+public class RandomDataGenerator {
+        }
+        for (int i = 0; i < 10; i++) {
+
+    public static void main(String[] args) {
+        int[] data = new int[10];
+        }    return data
+    for item in data:
+
+
+def generate_random_data():
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+
+if __name__ == "__main__":
     main()
         print(f"Random Number: {item}")
 
+import random
 def main():
-
-if __name__ == "__main__":
-    for item in data:function main() {
-function generateRandomData() {
-}
-}
-        data.push(Math.floor(Math.random() * 100) + 1);
-    }
-    });
-
-main();
-    const data = [];
-    data.forEach(item => {
-    const data = generateRandomData();
-    return data;
-
-
-    for (let i = 0; i < 10; i++) {
-        console.log("Random Number: " + item);
