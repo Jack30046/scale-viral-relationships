d574751bbfeec977f30fccc4321a7a79bcbe3eea
Jack <143327277+Jack30046@users.noreply.github.com>
2023-10-25 05:28:06 +0100

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,25 +1,14 @@
-            System.out.println("Random Number: " + item);
-    public static void main(String[] args) {
 
-        }
-        for (int item : data) {
-public class RandomDataGenerator {
+        print(f"Random Number: {item}")
+def main():
+import random
+    return data
+    for item in data:
+def generate_random_data():
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
 
-        }
-            data[i] = (int) (Math.random() * 100) + 1;
-}
-        for (int i = 0; i < 10; i++) {
-    }
-        int[] data = new int[10];        for (int i = 0; i < 10; i++) {
-    public static void main(String[] args) {
-        int[] data = new int[10];
-    }
 
-        for (int item : data) {
-        }
-}
+    main()
 
-        }
-public class RandomDataGenerator {
-            System.out.println("Random Number: " + item);
-            data[i] = (int) (Math.random() * 100) + 1;
+if __name__ == "__main__":
